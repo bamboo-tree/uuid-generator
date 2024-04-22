fb09ecf819fda1798e72d2d5353b534ce4010152
Maciej Kaminski <maciej.kam03@gmail.com>
2024-04-22 09:56:02 +0200

    Added useState so it doesn't reload whole page but just the component

--- src/components/Generator/Generator.tsx
+++ src/components/Generator/Generator.tsx
@@ -1,15 +1,17 @@
 import { v4 as uuidv4 } from "uuid";
 import { Text, Button } from "../../ui";
+import { useState } from "react";
 
 export const Generator = () => {
+	const [id, setId] = useState(uuidv4());
+
 	const handleClick = () => {
-		console.log("Click");
-		window.location.reload();
+		setId(uuidv4());
 	};
 
 	return (
 		<div>
-			<Text>{uuidv4()}</Text>
+			<Text>{id}</Text>
 			<Button label="Refresh" onClick={handleClick}></Button>
 		</div>
 	);
